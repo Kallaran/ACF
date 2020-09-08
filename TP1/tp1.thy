bb62de01e7d02b8d7c37276ef0ea8062be98606b
Derieux Jean <jean.derieux@etudiant.univ-rennes1.fr>
2020-09-08 05:19:57 +0200

    Add TP1 exercise 3

--- TP1/tp1.thy
+++ TP1/tp1.thy
@@ -23,6 +23,30 @@ lemma "\<not>(A \<and> B) \<longleftrightarrow> \<not>A \<or> \<not>B"
 
 
 
+(* Exercice 3*)
 
+lemma  "\<forall>(x::int) y  z. x + y > x + z \<longrightarrow> x + x > y + z"
+  nitpick
+  sorry
+(*formule satisfaisable si 2x >y+z*)
+lemma  "\<forall>(x::int) y  z.( x + y > x + z) \<and> (2 * x > y + z) \<longrightarrow> x + x > y + z"
+  apply auto
+  done
+
+lemma "\<forall>(x::int) y. x + y \<le> x * y"
+  nitpick
+  sorry
+(*formule satisfaisable si(x > 1) \<and> (y > 1)*)
+lemma "\<forall>(x::int) y.  (x > 1) \<and> (y > 1) \<longrightarrow> (x + y \<le> x * y) "
+  nitpick
+  sorry
+
+lemma "\<forall>(x::int)yz. x > y \<and> z > 0 \<longrightarrow> x * z > y * z" 
+  apply auto
+  done (*formule valide*)
+
+lemma "\<exists>x. P(f(x)) \<longrightarrow> (\<forall>x. P(f(x)))"
+  apply auto
+  done(*formule valide*)
 
 end
