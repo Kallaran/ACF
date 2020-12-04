645801a441c1453b3425c12494aa67487ce359e6
Derieux Jean <jean.derieux@etudiant.univ-rennes1.fr>
2020-12-04 05:09:52 +0100

    Added prop3

--- TP8-9/tp89.thy
+++ TP8-9/tp89.thy
@@ -1,5 +1,5 @@
 theory tp89
-imports Main "~~/src/HOL/Library/Code_Target_Nat" (* pour l'export Scala *)
+imports Main table "~~/src/HOL/Library/Code_Target_Nat" (* pour l'export Scala *)
 
 begin
 
@@ -15,9 +15,13 @@ datatype message=
 | Ack transid nat
 | Cancel transid
 
-datatype transBdd = list
 
 type_synonym transaction= "transid * nat"
+
+(* status correspond à (Valid, Amm, Amc, Canceled) *)
+type_synonym status= "bool * nat * nat * bool" 
+type_synonym transBdd = "(transid,  status) table"
+
 
 (* permet de mettre à jour une transBdd en fonction d'un nouveau message *)
 fun traiterMessage:: "message \<Rightarrow> transBdd  \<Rightarrow> transBdd"
@@ -30,10 +34,32 @@ fun export:: "transBdd \<Rightarrow> transaction list"
 "export transBdd = []"
 
 (* permet d'obtenir une transBdd à partir d'une liste de messages *)
-fun traiterMessageList:: "message list \<Rightarrow> transBdd"
+fun traiterMessageList:: " message list  \<Rightarrow> transBdd"
   where
-"(traiterMessageList (a#r)) = []"
+"(traiterMessageList [] ) = [] " |
+"(traiterMessageList (x#rs)) = (traiterMessage x (traiterMessageList rs))"
 
+
+(* Toutes les transactions validées ont un montant strictement supérieur à zero *)
+lemma prop1:"(List.member (export (traiterMessageList a)) (transid, montant)) \<longrightarrow> (montant > 0) "
+  nitpick[timeout=120]
+  sorry
+
+(* Toutes les transactions validées ont un montant strictement supérieur à zero *)
+lemma prop1bis:"(List.member (export bdd) (transid, montant)) \<longrightarrow> (montant > 0) "
+  nitpick[timeout=120]
+  sorry
+
+
+(* Dans la liste de transactions validées, tout triplet (c,m,i)  n'apparaît qu'une seule fois *)
+lemma prop2:"\<not>(List.member (List.remove1 (transid, montanty) (export (traiterMessageList a)) ) (transid, montantx) )  "
+  nitpick[timeout=120]
+  sorry
+
+
+(* Toute transaction (même validée) peut être annulée *)
+(* interprétée ici comme : si un message 'cancel transid' est reçu alors le export de la bdd ne montrera pas de transaction ayant ce transid *)
+lemma prop3:"\<not>(List.member ( export (traiterMessage (Cancel transid) (traiterMessageList a))) (transid, montant))"
 
 (* ----- Exportation en Scala (Isabelle 2018) -------*)
 
